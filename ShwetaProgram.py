e083e297590fbb89a552099365cbaf41c072016c
hbh7 <hbh7@hbh7.com>
2019-01-01 20:26:19 -0500

    Made a function thingy

--- ShwetaProgram.py
+++ ShwetaProgram.py
@@ -19,10 +19,22 @@ def iterateSign(sign):
         sign = "+"
     return sign
 
+def checkMath(algebraList, target):
+    print(algebraList)
+    evalString = ''.join(str(e) for e in algebraList)
+    evalResult = eval(evalString)
+    print("=" + str(evalResult))
+    print("Should be " + str(target))
+
+    if evalResult != target:
+        print("Math does not compute")
+    else:
+        print("Math checks out")
+
 
 print("Hello, world!")
 
-inputNumbers = [1, 2, 3, 4, 5, 15]
+inputNumbers = [1, 2, 3, 4, 5, 10]
 
 print("lol tough luck for you, no inputting your own numbers haha")
 print("(Don't worry, that'll come later)")
@@ -35,20 +47,15 @@ for number in inputNumbers:
     else:
         algebraList.pop()
 
+while checkMath(algebraList, inputNumbers[-1]) != inputNumbers[-1]:
+    for i in range(0, algebraList.count("+")):
+        if i == 0:
+            i = i + 1
+        else:
+            i = i + 2
 
-print(algebraList)
+        algebraList[i] = iterateSign(algebraList[i])
 
-evalString = ''.join(str(e) for e in algebraList)
-evalResult = eval(evalString)
-print("=" + str(evalResult))
-
-if evalResult != inputNumbers[-1]:
-    print("Math does not compute")
-else:
-    print("Math checks out")
-
-
-#while(evalResult != inputNumbers[-1]):
 
 
 
