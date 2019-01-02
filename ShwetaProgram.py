9fc24ffa6ef0a719f25c9d11a21f2aeeff672287
hbh7 <hbh7@hbh7.com>
2019-01-01 21:14:09 -0500

    Code cleanup

--- ShwetaProgram.py
+++ ShwetaProgram.py
@@ -10,18 +10,12 @@ def iterateSign(sign):
     return sign
 
 def checkMath(algebraList, target):
-    #print(algebraList)
     evalString = ''.join(str(e) for e in algebraList)
-    #print(evalString)
     evalResult = eval(evalString)
-    #print("=" + str(evalResult))
-    #print("Should be " + str(target))
 
     if int(evalResult) != int(target):
-        #print("Math does not compute")
         return False
     else:
-        #print("Math checks out")
         print(evalString + "=" + target)
         exit(0)
 
@@ -46,11 +40,6 @@ algebraList.pop()
 algebraList.pop()
 
 checkMath(algebraList, inputNumbers[-1])
-
-#for i in range(1, len(algebraList), 2):
-#    for k in range(0, 4):
-#        algebraList[i] = iterateSign(algebraList[i])
-#        checkMath(algebraList, inputNumbers[-1])
 
 cycleDown(algebraList, 1)
 
